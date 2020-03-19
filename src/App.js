0280e63df215ad630c8306750ac8dd77532c5968
Dan Slimmon <dslimmon@hashicorp.com>
2020-03-19 11:34:14 +0000

    moved Column into its own file

--- src/App.js
+++ src/App.js
@@ -1,47 +1,6 @@
 import React from 'react';
 import './App.css';
-import Card from './Card.js';
-
-class Column extends React.Component {
-  colName() {
-    switch (this.props.type) {
-      case "sym":
-        return "Symptoms";
-      case "hyp":
-        return "Hypotheses";
-      case "act":
-        return "Actions";
-      default:
-        console.log("Unknown column type '" + this.props.type + "' passed to <Column />");
-        return "Unknown Column";
-    }
-  }
-
-  render() {
-    let cardComponents = this.props.cards.map((cardData) => {return (
-      <Card
-        type={this.props.type}
-        key={cardData.cardID}
-        cardID={cardData.cardID}
-        title={cardData.title}
-        description={cardData.description}
-        selected={this.props.selectedCard === cardData.cardID}
-        surfaced={this.props.surfacedCards.includes(cardData.cardID)}
-        selectedCardInputValues={this.props.selectedCardInputValues}
-
-        onCardSelect={this.props.onCardSelect}
-        onCardChange={this.props.onCardChange}
-      />
-    )});
-
-    return (
-      <div className={"column column-" + this.props.type + " col-sm"}>
-        <h1>{this.colName()}</h1>
-        {cardComponents}
-      </div>
-    );
-  }
-}
+import Column from './Column.js';
 
 function CardOverlay(props) {
   return (
